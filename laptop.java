1943a0a9db5fb1d0a6485d5e38c51eef5656fd41
Samyam <139134689+samyam81@users.noreply.github.com>
2024-01-24 14:26:40 +0545

    Create laptop.java

---
+++ laptop.java
@@ -0,0 +1,21 @@
+public class laptop {
+    private int Lap_ID;
+    private String Lap_processor;
+
+    public String getLap_processor() {
+        return Lap_processor;
+    }
+
+    public void setLap_processor(String lap_processor) {
+        this.Lap_processor = lap_processor;
+    }
+
+    public int getLap_ID() {
+        return Lap_ID;
+    }
+
+    public void setLap_ID(int Lap_ID) {
+        this.Lap_ID = Lap_ID;
+    }
+
+}
